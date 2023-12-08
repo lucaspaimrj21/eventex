64edd0e81d18a2a2c731c7ac7e73aa840610e9b0
Lucas Paim <lucaspaimrj21@gmail.com>
2023-12-07 23:47:43 -0300

    mti

--- eventex/core/admin.py
+++ eventex/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.db.models.query import QuerySet
 from django.utils.html import format_html
-from eventex.core.models import Contact, Course, Speaker, Talk
+from eventex.core.models import Contact, Speaker, Talk, Course
 
 class ContactInline(admin.TabularInline):
     model = Contact
@@ -31,7 +32,11 @@ class SpeakerModelAdmin(admin.ModelAdmin):
       
     phone.short_description = 'telefone'
 
+class TalkModelAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(course=None)
 
 admin.site.register(Speaker, SpeakerModelAdmin)
-admin.site.register(Talk)
+admin.site.register(Talk, TalkModelAdmin)
 admin.site.register(Course)
--- eventex/core/tests/test_model_talk.py
+++ eventex/core/tests/test_model_talk.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from eventex.core.managers import PeriodManager
-from eventex.core.models import Talk, Course
+from eventex.core.models import Course, Talk
 
 class TalkModelTest(TestCase):
     def setUp(self):
@@ -39,6 +39,9 @@ class TalkModelTest(TestCase):
 
     def test_str(self):
         self.assertEqual('Título da Palestra', str(self.talk))
+
+    def test_ordering(self):
+        self.assertListEqual(['start'], Talk._meta.ordering)
 
 class PeriodoManagerTest(TestCase):
     def setUp(self):
@@ -85,3 +88,6 @@ class CourseModelTest(TestCase):
     
     def test_manager(self):
         self.assertIsInstance(Course.objects, PeriodManager)
+
+    def test_ordering(self):
+        self.assertListEqual(['start'], Course._meta.ordering)
